2b6d5523e61b05a7bd5958046ef855d15f2e29e0
Wayne Brantley <waynebrantley@users.noreply.github.com>
2022-02-14 15:26:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ try {
     const { GITHUB_REF, GITHUB_RUN_NUMBER } = process.env
     const releaseBranch = core.getInput('release-branch')
     const today = new Date()
-    const localVersion = `${today.getFullYear()}.${today.getMonth() + 1}.${today.getDay()}.${GITHUB_RUN_NUMBER}`
+    const localVersion = `${today.getFullYear()}.${today.getMonth() + 1}.${today.getDate()}.${GITHUB_RUN_NUMBER}`
     const branch = /[^/]*$/.exec(GITHUB_REF)[0]
     core.info(branch)
     core.info(releaseBranch)
