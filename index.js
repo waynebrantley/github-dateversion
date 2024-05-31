20c0ad5daab3d88585f496fd75fba9578841fa31
Wayne Brantley <wayne@miniter.com>
2024-05-31 08:21:24 -0400

    support branch names in PR;  Use simplified GITHUB_REF_NAME

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const core = require('@actions/core')
 
 try {
-    const { GITHUB_REF, GITHUB_RUN_NUMBER } = process.env
+    const { GITHUB_RUN_NUMBER, GITHUB_HEAD_REF, GITHUB_REF_NAME } = process.env
     const releaseBranch = core.getInput('release-branch')
     const today = new Date()
     const localVersion = `${today.getFullYear()}.${today.getMonth() + 1}.${today.getDate()}.${GITHUB_RUN_NUMBER}`
-    const branch = /[^/]*$/.exec(GITHUB_REF)[0]
-    core.info(branch)
+    
+    // const branch = /[^/]*$/.exec(GITHUB_REF)[0]  //GITHUB_REF out of process.env
+    // core.info(branch)
+    const branchName = GITHUB_HEAD_REF || GITHUB_REF_NAME
+    core.info(branchName)
     core.info(releaseBranch)
-    if (branch == releaseBranch)
+    if (branchName == releaseBranch)
         core.setOutput('version', localVersion)
     else
-        core.setOutput('version', `${localVersion}-${branch.substring(0, 20)}`)
+        core.setOutput('version', `${localVersion}-${branchName.substring(0, 20)}`)
 } catch (error) {
     core.setFailed(error.message)
 }
